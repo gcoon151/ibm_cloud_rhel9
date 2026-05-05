87273f0c072be2288c66a885f0a42c33de64725c
Gerald Coon <grcoon@us.ibm.com>
2026-05-05 15:45:55 -0400

    Add old kernel removal to coco-components.sh
    - Remove old kernel during PodVM build (not in kickstart)
    - Uses virt-customize --run-command before podvm_maker.sh
    - Handles CVE-2026-31431 by removing vulnerable kernel
    - Uses --setopt=protected_packages= to override kernel-modules-core protection
    - Only removes if multiple kernels present

--- scripts/coco/coco-components.sh
+++ scripts/coco/coco-components.sh
@@ -120,12 +120,31 @@ fi
 
 # Enable debug mode to see what virt-customize is doing
 export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
+
+# Remove old kernel if multiple kernels are present (CVE-2026-31431 fix)
+# This handles the case where kickstart installed both old and new kernels
+echo "Checking for old kernels to remove..."
+OLD_KERNEL_REMOVAL="
+KERNEL_COUNT=\$(rpm -q kernel-uki-virt | wc -l)
+if [ \$KERNEL_COUNT -gt 1 ]; then
+    echo 'Multiple kernels found, removing old kernel...'
+    OLD_KERNEL=\$(rpm -q kernel-uki-virt | sort -V | head -n 1)
+    OLD_VERSION=\$(echo \$OLD_KERNEL | sed 's/kernel-uki-virt-//')
+    echo \"Removing: \$OLD_KERNEL and kernel-modules-core-\$OLD_VERSION\"
+    dnf remove -y --setopt=protected_packages= kernel-uki-virt-\$OLD_VERSION kernel-modules-core-\$OLD_VERSION || true
+    echo 'Old kernel removal complete'
+else
+    echo 'Only one kernel found, no removal needed'
+fi
+"
+
 virt-customize \
     --copy-in $ARTIFACTS_FOLDER/podvm-binaries.tar.gz:/tmp/ \
     --copy-in $ARTIFACTS_FOLDER/pause-bundle.tar.gz:/tmp/ \
     --copy-in $ARTIFACTS_FOLDER/luks-config.tar.gz:/tmp/ \
     ${UPTYCS_COPY_ARGS} \
     ${METRICS_COPY_ARGS} \
+    --run-command "$OLD_KERNEL_REMOVAL" \
     --run $ARTIFACTS_FOLDER/podvm_maker.sh \
     ${UPTYCS_RUN_ARGS} \
     ${METRICS_RUN_ARGS} \
